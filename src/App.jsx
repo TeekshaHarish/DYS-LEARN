10f5d393673751e5928213a90499eb0033f805d8
sukhdeep5443 <sukhdeep9c@gmail.com>
2024-02-06 00:01:53 +0530

    message

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,22 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Footer from "./components/Footer/Footer";
+import Demo from "./Demo/Demo";
 
 function App() {
   return (
     <Router>
-      {/* <Routes>
-        <Route path='/' element={<Login />}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/home' element={<Dashboard />}/>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Dashboard />} />
       </Routes>
       <Footer />
-      <Toaster position='top-center' />
+      <Toaster position="top-center" />
+      <Dashboard />
+      <Demo />
     </Router>
-  )
+  );
 }
 
 export default App;
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,17 +1,17 @@
-import React, { useState, useRef } from 'react';
-import { FaUserCircle } from "react-icons/fa"
-import { useNavigate } from 'react-router-dom';
-import { useAuthContext } from '../../hooks/useAuthContext';
-import Sidebar from '../../components/Sidebar/Sidebar';
-import Notes from '../../components/Notes/Notes';
-import TextToSpeech from '../../components/TextToSpeech/TextToSpeech';
-import Summary from '../../components/Summary/Summary';
-import SpeechToText from '../../components/SpeechToText/SpeechToText';
-import FlashCards from '../../components/FlashCards/FlashCards';
+import React, { useState, useRef } from "react";
+import { FaUserCircle } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Notes from "../../components/Notes/Notes";
+import TextToSpeech from "../../components/TextToSpeech/TextToSpeech";
+import Summary from "../../components/Summary/Summary";
+import SpeechToText from "../../components/SpeechToText/SpeechToText";
+import FlashCards from "../../components/FlashCards/FlashCards";
 import "./Dashboard.css";
 
 const Dashboard = () => {
-  const [activeComponent, setActiveComponent] = useState('notes');
+  const [activeComponent, setActiveComponent] = useState("notes");
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef(null);
   const { dispatch, user } = useAuthContext();
@@ -23,7 +23,7 @@ const Dashboard = () => {
 
   const handleButtonClick = (componentName) => {
     setActiveComponent(componentName);
-  }
+  };
 
   const handleLogout = () => {
     dispatch({ type: "LOGOUT" });
@@ -31,28 +31,33 @@ const Dashboard = () => {
   };
 
   return (
-    <div className='dashboard__page__container'>
-      <div className='dashboard__header'>
-        <h1>
-          🙌Welcome Back,{" "}
-          {user && <span>{user.name}</span>}
-        </h1>
+    <div className="dashboard__page__container">
+      <div className="dashboard__header">
+        <h1>🙌Welcome Back {user && <span>{user.name}</span>}</h1>
 
-        <div className='dashboard__header__icons__container'>
-          <FaUserCircle className='dashboard__header__icons' onClick={toggleDropdown} />
-          <div style={{ display: isOpen ? "block" : "none" }} className="user__dropdown" ref={dropdownRef} onClick={handleLogout}>
+        <div className="dashboard__header__icons__container">
+          <FaUserCircle
+            className="dashboard__header__icons"
+            onClick={toggleDropdown}
+          />
+          <div
+            style={{ display: isOpen ? "block" : "none" }}
+            className="user__dropdown"
+            ref={dropdownRef}
+            onClick={handleLogout}
+          >
             Logout
           </div>
         </div>
       </div>
-      <div className='dashboard__page__main'>
+      <div className="dashboard__page__main">
         <Sidebar handleButtonClick={handleButtonClick} />
-        <div className='main__content__container'>
-          {activeComponent === 'notes' && <Notes />}
-          {activeComponent === 'stt' && <SpeechToText />}
-          {activeComponent === 'summary' && <Summary />}
-          {activeComponent === 'tts' && <TextToSpeech />}
-          {activeComponent === 'cards' && <FlashCards />}
+        <div className="main__content__container">
+          {activeComponent === "notes" && <Notes />}
+          {activeComponent === "stt" && <SpeechToText />}
+          {activeComponent === "summary" && <Summary />}
+          {activeComponent === "tts" && <TextToSpeech />}
+          {activeComponent === "cards" && <FlashCards />}
         </div>
       </div>
     </div>
