fa2548c0ef99aa2720cb1c9693fd744b3ff2ae56
TeekshaHarish <102624456+TeekshaHarish@users.noreply.github.com>
2024-02-06 09:50:49 +0530

    Chnages

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Footer from "./components/Footer/Footer";
-import Match from "./Match/Match";
+import Match from "./components/Match/Match";
 import Dictation from "./components/Dictation/Dictation";
 
 function App() {
@@ -13,13 +13,11 @@ function App() {
     <Router>
       {/* <Register /> */}
       <Routes>
-
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Dashboard />} />
       </Routes>
 
-     
       <Footer />
       <Toaster position="top-center" />
     </Router>
